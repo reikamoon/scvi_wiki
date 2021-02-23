c18216122add2944165839b95df7bfb5aecfe8ff
Anjelica <ambrosio.anjelica@gmail.com>
2021-02-22 20:34:54 -0800

    created mock array of characters

--- app.js
+++ app.js
@@ -12,10 +12,22 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main', handlebars: allowInsecu
 // Use handlebars to render
 app.set('view engine', 'handlebars');
 
+// OUR MOCK ARRAY OF PROJECTS
+var characters = [
+  { name: "Grøh", weapontype: "Twinblade", weapon: "Arondight Replica", fightstyle: "Aval Dual-Swordsmanship", difficulty: "Beginner", birthplace: "Kingdom of Denmark-Norway", birthday: "November 18", sex: "Male", height: "5'11 (181 cm)", weight: "180lbs (82kg)", bloodtype: "A"},
+  { name: "Sophitia Alexandra", weapontype: "Sword and Shield", weapon: "Omega Sword and Elk Shield", fightstyle: "Athenian Style", difficulty: "Beginner", birthplace: "Athens, Ottoman Empire", birthday: "March 12", sex: "Female", height: "5'6 (168cm)", weight: "Will not Reveal", bloodtype: "B" }
+]
+
+
 // Tell our app to send the "hello world" message to our home page
 // Render the "home" layout for the main page and send the following msg
 app.get('/', (req, res) => {
   res.render('home', { msg: 'Handlebars are Cool!' });
+})
+
+// INDEX
+app.get('/cast', (req, res) => {
+  res.render('characters-index', { characters: characters });
 })
 
 // Choose a port to listen on
